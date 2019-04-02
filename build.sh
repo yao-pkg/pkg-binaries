af14c711c55de2ad6942c9b834ef48cded9e926b
Daniel Lando <daniel.sorridi@gmail.com>
2019-04-02 14:23:15 +0200

    Testing cross-build scripts

--- build.sh
+++ build.sh
@@ -9,12 +9,14 @@ select option in "${options[@]}"; do
 	case "$REPLY" in
 		1)
             arch=arm32v6
-            folder=armv7
+            pkg_arch=armv7
+			qemu=arm
 			break
 			;;
 		2)
             arch=arm64v8
-            folder=arm64
+            pkg_arch=arm64
+			qemu=aarch64
 			break
 			;;
 		*)
@@ -25,9 +27,73 @@ select option in "${options[@]}"; do
 	esac
 done
 
-sudo docker run --rm --privileged multiarch/qemu-user-static:register
-docker build -f Dockerfile.${arch} -t robertslando/pkgbinaries:${arch}-latest .
+PS3="NodeJS version: >"
+options=(
+	"8.11.3"
+	"10.4.1"
+)
+echo ''
 
-rm Dockerfile.${arch}
+select option in "${options[@]}"; do
+	case "$REPLY" in
+		1)
+            node_version=8.11.3
+			pkg_node=node8
+			break
+			;;
+		2)
+            node_version=10.4.1
+			pkg_node=node10
+			break
+			;;
+		*)
+			echo '####################'
+			echo '## Invalid option ##'
+			echo '####################'
+			exit
+	esac
+done
 
-docker cp $(docker ps -a -q | head -n 1):~/fetched-v8.11.3-linux-${arch} ./${folder}/fetched-v8.11.3-linux-${arch}
+PS3="OS version: >"
+options=(
+	"linux"
+	"alpine"
+)
+echo ''
+
+select option in "${options[@]}"; do
+	case "$REPLY" in
+		1)
+			pkg_os=linux
+			break
+			;;
+		2)
+			pkg_os=alpine
+            os_version=-alpine
+			break
+			;;
+		*)
+			echo '####################'
+			echo '## Invalid option ##'
+			echo '####################'
+			exit
+	esac
+done
+
+# sudo docker run --rm --privileged multiarch/qemu-user-static:register
+cp Dockerfile.cross Dockerfile.build
+
+sed -i "s|__ARCH__|${arch}|g" Dockerfile.build
+sed -i "s|__NODE_VERSION__|${node_version}|g" Dockerfile.build
+sed -i "s|__OS_VERSION__|${os_version}|g" Dockerfile.build
+sed -i "s|__QEMU__|${qemu}|g" Dockerfile.build
+
+sed -i "s|__NODE_PKG__|${pkg_node}|g" Dockerfile.build
+sed -i "s|__PKG_OS__|${pkg_os}|g" Dockerfile.build
+sed -i "s|__PKG_ARCH__|${pkg_arch}|g" Dockerfile.build
+
+docker build -f Dockerfile.build -t ${arch}/pkgbinaries:${pkg_os}-${pkg_node} .
+docker run -it -d ${arch}/pkgbinaries:${pkg_os}-${pkg_node} sh
+docker cp $(docker ps -a -q | head -n 1):/fetched-v${node_version}-${pkg_os}-${pkg_arch} ./${pkg_arch}/fetched-v${node_version}-${pkg_os}-${pkg_arch}
+
+rm Dockerfile.build
